9a0cf76c2906e5ff27342b9489c67830a233d47d
Jakia-Noor <94386843+Jakia-Noor@users.noreply.github.com>
2022-02-02 09:22:42 +0600

    Add files via upload

---
+++ Register.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <titel>REGISTRATION FORM</titel>
+</head>
+<body>
+    <form>
+        <table>
+            <tr>
+                <td> First Name: </td>
+                <td> <input type="text" name="First Name"></td>
+                 </tr>
+                  <tr>
+                      <td> Last Name: </td>
+                      <td> <input type="text" name="Last Name"></td>
+                       </tr>
+</body>
+</form>
+</table>
+</html>
+
+                    
+
