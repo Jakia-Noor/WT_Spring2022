862bd4bf376bf8be19e8e074424610098e460289
Jakia-Noor <94386843+Jakia-Noor@users.noreply.github.com>
2022-02-02 12:19:55 +0600

    Add files via upload

---
+++ Task 1/Register.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <titel>REGISTRATION FORM</titel>
+</head>
+<body>
+    <form>
+        <table>
+            <tr>
+                <td> First Name: </td>
+                <td> <input type="text" name="First Name"></td>
+                 </tr>
+                  <tr>
+                      <td> Last Name: </td>
+                      <td> <input type="text" name="Last Name"></td>
+                       </tr>
+                       <tr>
+                       <td> Age:</td> 
+                      <td> <input type="Number"></td><br>
+
+                       </tr>
+
+                      <tr>
+                      <td> Designation:</td>
+                      <td>
+                       <input type="radio" name="Designation">Junior programmer
+                       <input type="radio" name="Designation">Senior programmer
+                       <input type="radio" name="Designation">project lead
+
+                       <br>
+
+                       <tr>
+                      <td> Preferred language:</td>
+                      <td>
+                       <input type="checkbox">JAVA
+                       <input type="checkbox">PHP
+                       <input type="checkbox">C++
+                        </td>
+
+                       <tr>
+                           <td> Email: </td>
+                           <td> <input type="mail" name=" "></td>
+                       </tr> 
+                       <tr>
+                           <td> Password: </td>
+                           <td><input type="password" name=" "></td>
+                        </tr>
+                        <tr>
+                            <td> Please choose a file</td>
+                            <td> <input type="text" placeholder="choose file" name=" "></td>
+                            <td>No file chosen</td>
+                        </tr>
+                         
+                         <td>
+                        <input type="submit" value="Submit">
+                        <input type ="reset" value="Reset">
+                         </td>
+
+
+
+</body>
+</form>
+</table>
+</html>
+
+                    
+
